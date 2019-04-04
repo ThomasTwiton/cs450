9988f727d6d0a4cffa0acc223e969388f8bae4de
Thomas Twiton <twitth01@luther.edu>
2019-04-03 21:43:06 -0700

    project 3 bean board

--- quincunx/quincunx.py
+++ quincunx/quincunx.py
@@ -16,14 +16,18 @@ class Board:
     Contains multiple levels of pegs
     """
 
-    def __init__(self, bins: int):
+    def __init__(self, bins: int, start: int):
         """Make a new board of the specified size"""
         self._bins = [0] * bins
-        self._pegs = bins // 2
+        self._pegs = start
 
-    def status(self, pos: int):
+    def status(self):
         """Print status"""
-        raise NotImplementedError
+        sum = 0
+        for bin in self._bins:
+            sum += bin
+            print("|{:^5}".format(bin), end='')
+        print("|  " + str(sum))
 
     def __len__(self):
         """Return the board size"""
@@ -52,35 +56,97 @@ class Bean(threading.Thread):
 
     def __init__(self, board: object, start: int, prob: float, lock: object):
         """Make a new Bean"""
-        raise NotImplementedError
+        threading.Thread.__init__(self, target=self.run)
+        self.galton = board
+        self.position = start
+        self.probability = prob
+        self.lock = lock
+
 
     def move_left(self):
         """Move a bean left"""
-        raise NotImplementedError
+        with self.lock:
+            self.galton[self.position] -= 1
+            if self.position > 0:
+                self.position -= 1
+            self.galton[self.position] += 1
+            
 
     def move_right(self):
         """Move a bean right"""
-        raise NotImplementedError
+        with self.lock:
+            self.galton[self.position] -= 1
+            if self.position < (self.galton.__len__() -1):
+                self.position += 1
+            self.galton[self.position] += 1
+            
 
     def run(self):
         """Run a bean through the pegs"""
-        raise NotImplementedError
-
+        for r in range(self.galton.pegs):
+            chance = random.random()
+            if chance < self.probability/2:
+                self.move_left()
+            elif chance < self.probability:
+                self.move_right()
 
 def main():
     """Main function"""
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Process the arguments.")
+    parser.add_argument("--beans", help="Number of beans to run through the board. Default = 1000")
+    parser.add_argument("--bins", help="Number of bins the board has. Default = 11")
+    parser.add_argument("--start", help="Layers of pins the beans will go through. Default = 5")
+    parser.add_argument("--prob", help="Probability the bean will bounce left. Default = 0.5")
+
+    args = parser.parse_args()
+    if args.beans == None:
+        beans = 1000
+    else:
+        beans = args.beans
+    if args.bins == None:
+        bins = 11
+    else:
+        bins = args.bins
+    if args.start == None:
+        start = 5
+    else:
+        start = args.start
+    if args.prob == None:
+        prob = 0.5
+    else:
+        prob = args.prob
 
     print("Start")
     # Create a list of jobs
+    bean_jobs = []
+    
     # Create a shared lock
+    lock = threading.Lock()
     # Create a board
+    galton = Board(bins, start)
+    middle_bin = galton._bins.__len__()//2
+    galton._bins[middle_bin] = beans
+
     # Create jobs (beans)
+    for i in range(beans):
+        b = Bean(galton, middle_bin, prob, lock)
+        bean_jobs.append(b)
+
     # Print the board status
+    galton.status()
+
     # Start jobs
+    for bean in bean_jobs:
+        bean.start()
+
     # Stop jobs
+    for bean in bean_jobs:
+        bean.join()
+
     # Print the board status
+    galton.status()
+
     print("Done")
 
 
